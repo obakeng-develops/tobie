50c86578870a94905fff1e2536f604993fb4637b
Obakeng <mosadiobakeng7@gmail.com>
2022-04-26 16:34:00 +0200

    Added pydantic model creator

--- server/v1/models/models.py
+++ server/v1/models/models.py
@@ -1,13 +1,25 @@
 from tortoise.models import Model
 from tortoise import fields, Tortoise
+from tortoise.contrib.pydantic import pydantic_model_creator
 
 
 class User(Model):
+    """
+    The user model
+    """
     id = fields.IntField(pk=True)
     name = fields.CharField(max_length=255)
 
-    def __str__(self):
+    def full_name(self) -> str:
         return self.name
+
+    class PydanticMeta:
+        computed = ['full_name']
+
+
+User_Pydantic = pydantic_model_creator(User, name="User")
+UserIn_Pydantic = pydantic_model_creator(
+    User, name="User", exclude_readonly=True)
 
 
 async def init():
