f0eee7243cf9de4085250b80991f20f2d8b3e582
Obakeng <mosadiobakeng7@gmail.com>
2022-04-26 16:58:07 +0200

    Added UserLogin model

--- server/v1/models/models.py
+++ server/v1/models/models.py
@@ -3,18 +3,27 @@ from tortoise import fields, Tortoise
 from tortoise.contrib.pydantic import pydantic_model_creator
 
 
-class User(Model):
+class UserLogin(Model):
     """
-    The user model
+    The user login model
     """
     id = fields.IntField(pk=True)
-    name = fields.CharField(max_length=255)
+    first_name = fields.CharField(max_length=255, null=True)
+    last_name = fields.CharField(max_length=255, null=True)
+    email = fields.CharField(max_length=80)
+    password_hash = fields.CharField(max_length=128, null=True)
+    join_date = fields.DatetimeField(auto_now_add=True)
+    modified_at = fields.DatetimeField(auto_now_add=True)
 
     def full_name(self) -> str:
-        return self.name
+        """
+        Returns the full name of the user
+        """
+        return self.first_name + " " + self.last_name
 
     class PydanticMeta:
         computed = ['full_name']
+        exclude = ['password_hash']
 
 
 User_Pydantic = pydantic_model_creator(User, name="User")
