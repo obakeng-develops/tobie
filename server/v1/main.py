7c6c4d585d8635dc4d975dc4bc775c6712865acf
Obakeng <mosadiobakeng7@gmail.com>
2022-04-26 16:51:48 +0200

    Removed unnecessary routes

--- server/v1/main.py
+++ server/v1/main.py
@@ -8,17 +8,6 @@ from tortoise.contrib.fastapi import HTTPNotFoundError, register_tortoise
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Optional[bool] = None
-
-
-@app.get("/v1/")
-def read_root():
-    return {"Hello": "World"}
-
-
 @app.get("/v1/users", response_model=List[User_Pydantic])
 async def get_users():
     return await User_Pydantic.from_queryset(User.all())
@@ -28,17 +17,6 @@ async def get_users():
 async def create_user(user: UserIn_Pydantic):
     user_obj = await User.create(**user.dict(exclude_unset=True))
     return await User_Pydantic.from_tortoise_orm(user_obj)
-
-
-@app.get("/v1/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.put("/v1/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_price": item.price, "item_id": item_id}
-
 
 register_tortoise(
     app,
