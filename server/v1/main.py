49e5df62149c5315bcd1fbe0aa7eaeb6fd2cf3ab
Obakeng <mosadiobakeng7@gmail.com>
2022-04-30 16:39:04 +0200

    Added two product routes - get all and create product

--- server/v1/main.py
+++ server/v1/main.py
@@ -38,6 +38,17 @@ async def create_user(user: UserIn_Pydantic):
 
 # Product Routes
 
+
+@app.get("/v1/product/all", response_model=List[Product_Pydantic])
+async def get_products():
+    return await Product_Pydantic.from_queryset(Product.all())
+
+
+@app.post("/v1/product/", response_model=Product_Pydantic)
+async def create_product(product: Product_Pydantic):
+    product_obj = await Product.create(**product.dict(exclude_unset=True))
+    return await Product_Pydantic.from_tortoise_orm(product_obj)
+
 # Store Routes
 
 # Notification Routes
