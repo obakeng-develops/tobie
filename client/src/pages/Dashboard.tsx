76238cdae812881f4bf95db4330a5388c719e954
Obakeng <mosadiobakeng7@gmail.com>
2022-06-01 16:14:36 +0200

    Added logo to dashboard

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 function Dashboard() {
   return (
     <div className='flex'>
-        <div className='top-0 bg-slate-800 h-screen w-64 text-white p-6'>Yebo</div>
+        <div className='top-0 bg-slate-800 h-screen w-64 text-white p-6'>
+            <img src='/images/tobie-logo.png' className='w-20 h-20'/>
+        </div>
         <div>Yo</div>
     </div>
   )
