e2852226bc0ecc717b7dd691c5a87837b1eefdf5
Obakeng <mosadiobakeng7@gmail.com>
2022-06-01 16:16:44 +0200

    Added nav elements

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -5,6 +5,12 @@ function Dashboard() {
     <div className='flex'>
         <div className='top-0 bg-slate-800 h-screen w-64 text-white p-6 flex flex-col items-center'>
             <img src='/images/tobie-logo.png' className='w-20 h-20'/>
+            <div>
+                <ul>
+                    <li className='text-2xl'>Profile</li>
+                    <li className='text-2xl'>Settings</li>
+                </ul>
+            </div>
         </div>
         <div>Yo</div>
     </div>
