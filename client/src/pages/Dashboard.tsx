ab536730c8e73e4f9c7936ba31b607934869f322
Obakeng <mosadiobakeng7@gmail.com>
2022-06-01 16:11:49 +0200

    Created sidebar for dashboard

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 function Dashboard() {
   return (
-    <div>
-        Hello
+    <div className='flex'>
+        <div className='top-0 bg-slate-800 h-full w-64 text-white p-6'>Yebo</div>
+        <div>Yo</div>
     </div>
   )
 }
