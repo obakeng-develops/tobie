4fa7a782f70a5778d80ec38c4a83c77cf862ca82
Obakeng <mosadiobakeng7@gmail.com>
2022-06-01 16:15:06 +0200

    Aligned to center

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Dashboard() {
   return (
     <div className='flex'>
-        <div className='top-0 bg-slate-800 h-screen w-64 text-white p-6'>
+        <div className='top-0 bg-slate-800 h-screen w-64 text-white p-6 flex flex-col items-center'>
             <img src='/images/tobie-logo.png' className='w-20 h-20'/>
         </div>
         <div>Yo</div>
