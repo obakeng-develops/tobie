27d490c9fe265d8c3c7ccf22a24cd767df90052e
Obakeng <mosadiobakeng7@gmail.com>
2022-06-01 15:56:20 +0200

    Added Dashboard to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Dashboard/>
     </div>
   );
 }
