89d46d6e91c85947b0fe8a54a4cc3fe4b8ef2a96
Obakeng <mosadiobakeng7@gmail.com>
2022-06-01 16:17:56 +0200

    Added vertical spacing between elements

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 function Dashboard() {
   return (
     <div className='flex'>
-        <div className='top-0 bg-slate-800 h-screen w-64 text-white p-6 flex flex-col items-center'>
+        <div className='top-0 bg-slate-800 h-screen w-64 text-white p-6 flex flex-col items-center space-y-16'>
             <img src='/images/tobie-logo.png' className='w-20 h-20'/>
             <div>
-                <ul>
+                <ul className='space-y-4'>
                     <li className='text-2xl'>Profile</li>
                     <li className='text-2xl'>Settings</li>
                 </ul>
