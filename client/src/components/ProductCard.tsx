55960ce3cbaca05e049bdead2cafc24cc860e209
Obakeng <mosadiobakeng7@gmail.com>
2022-06-08 11:51:49 +0200

    Changed text formatting

--- client/src/components/ProductCard.tsx
+++ client/src/components/ProductCard.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 function ProductCard() {
   return (
     <div className='rounded-md'>
-        <img src='/images/ryan-plomp.jpg' className='h-64 w-64'/>
+        <img src='/images/ryan-plomp.jpg' className='h-64 w-full'/>
         <div className='flex flex-col my-4 space-y-2'>
-            <label>Nike Airforce 1</label>
-            <label>R499</label>
-            <button className='bg-neutral-900 rounded-md text-white'>Buy</button>
+            <label className='text-2xl'>Nike Airforce 1</label>
+            <label className='text-2xl'>R499</label>
+            <button className='bg-neutral-900 text-2xl p-1 rounded-md text-white'>Buy</button>
         </div>
     </div>
   )
