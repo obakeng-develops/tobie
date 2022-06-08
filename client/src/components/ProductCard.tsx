e25e8f78bfc1a6df2aab9daeaee3175a3cc5699f
Obakeng <mosadiobakeng7@gmail.com>
2022-06-08 11:55:24 +0200

    Changed formatting of product card

--- client/src/components/ProductCard.tsx
+++ client/src/components/ProductCard.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 function ProductCard() {
   return (
-    <div className='rounded-md'>
-        <img src='/images/ryan-plomp.jpg' className='h-64 w-full'/>
-        <div className='flex flex-col my-4 space-y-2'>
-            <label className='text-2xl'>Nike Airforce 1</label>
-            <label className='text-2xl'>R499</label>
-            <button className='bg-neutral-900 text-2xl p-1 rounded-md text-white'>Buy</button>
+    <div className='rounded-lg shadow-lg'>
+        <img src='/images/ryan-plomp.jpg' className='h-64 w-full rounded-t-lg'/>
+        <div className='flex flex-col my-2 space-y-2 p-3'>
+            <label className='text-xl'>Nike Airforce 1</label>
+            <label className='text-xl'>R499</label>
+            <button className='bg-neutral-900 text-xl p-1 rounded-md text-white'>Buy</button>
         </div>
     </div>
   )
