40ed5797fb6f608653755d16ef4ed332ce0e36a0
Obakeng <mosadiobakeng7@gmail.com>
2022-06-08 11:42:47 +0200

    Created base template for product

--- client/src/components/ProductCard.tsx
+++ client/src/components/ProductCard.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function ProductCard() {
   return (
-    <div>ProductCard</div>
+    <div className='rounded-md'>
+        <img src='/images/ryan-plomp.jpg' className='h-64 w-64'/>
+        <div className='flex flex-col my-4 space-y-2'>
+            <label>Nike Airforce 1</label>
+            <label>R499</label>
+            <button className='bg-neutral-900 rounded-md text-white'>Buy</button>
+        </div>
+    </div>
   )
 }
 
