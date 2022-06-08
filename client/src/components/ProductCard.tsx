895e40f26837a6713fea689ccd276ad421d2d2c8
Obakeng <mosadiobakeng7@gmail.com>
2022-06-08 11:58:48 +0200

    Added percentage pointer on product card

--- client/src/components/ProductCard.tsx
+++ client/src/components/ProductCard.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 function ProductCard() {
   return (
-    <div className='rounded-lg shadow-lg'>
+    <div className='rounded-lg shadow-lg relative'>
         <img src='/images/ryan-plomp.jpg' className='h-64 w-full rounded-t-lg'/>
+        <label className='absolute top-2 right-2 text-base bg-white p-2 rounded-md'>25% off</label>
         <div className='flex flex-col my-2 space-y-2 p-3'>
             <label className='text-xl'>Nike Airforce 1</label>
             <label className='text-xl'>R499</label>
