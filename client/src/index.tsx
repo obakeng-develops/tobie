2e340e872a05da2ef6ae48387e70989ef03ffac5
Obakeng <mosadiobakeng7@gmail.com>
2022-06-13 08:52:50 +0200

    Imported react router components

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Dashboard from './pages/Dashboard';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
