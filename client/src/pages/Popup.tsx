77d902576a699008b44e8ceb36074bcd118efc98
Obakeng <mosadiobakeng7@gmail.com>
2022-06-14 08:45:29 +0200

    Created base template for product popup

--- client/src/pages/Popup.tsx
+++ client/src/pages/Popup.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
 
 function Popup() {
+
   return (
-    <div>Popup</div>
+    <div className='shadow-lg p-6 w-1/4 rounded-lg'>
+      <div className='flex justify-between'>
+        <label className='font-overpass font-bold'>Save Product</label>
+        <div>
+          <i>H</i>
+          <i>P</i>
+          <i>X</i>
+        </div>
+      </div>
+      <div>
+        <div></div>
+        <div className='flex flex-col font-overpass'>
+          <label>Nike Air Max</label>
+          <label>R475</label>
+        </div>
+        <div>
+          <button className='bg-black font-overpass text-white p-2 rounded-md w-full my-3'>Save</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
