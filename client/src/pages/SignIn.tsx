4bc7ed56fb48e230a842695506623f337d7833a0
Obakeng <mosadiobakeng7@gmail.com>
2022-06-17 08:59:29 +0200

    Changed background color

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -5,7 +5,7 @@ function SignIn() {
     <div className='flex flex-col justify-center items-center'>
         <img src='/images/tobie-logo.png' className='w-24 h-24'/>
         <label className='font-overpass font-bold text-2xl'>Sign In</label>
-        <button>Login with Facebook</button>
+        <button className='bg-blue-700 p-2 font-overpass text-white'>Login with Facebook</button>
     </div>
   )
 }
