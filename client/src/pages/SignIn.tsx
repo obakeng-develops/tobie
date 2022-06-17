37873440da2b334cc198d39d91a6a11d8f6c1d8b
Obakeng <mosadiobakeng7@gmail.com>
2022-06-17 09:04:48 +0200

    Added <hr> for content separation

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -9,6 +9,7 @@ function SignIn() {
             <button className='bg-blue-700 p-2 font-overpass text-white'>Login with Facebook</button>
             <button className='bg-rose-600 p-2 font-overpass text-white'>Login with Google</button>
         </div>
+        <hr></hr>
     </div>
   )
 }
