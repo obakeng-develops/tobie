069bf554ee69d3fb0f43f9e7e7eca90e089a1d93
Obakeng <mosadiobakeng7@gmail.com>
2022-06-17 09:02:44 +0200

    Changed y-axis spacing between buttons

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -5,7 +5,7 @@ function SignIn() {
     <div className='flex flex-col justify-center items-center'>
         <img src='/images/tobie-logo.png' className='w-24 h-24'/>
         <label className='font-overpass font-bold text-2xl'>Sign In</label>
-        <div className='flex flex-col'>
+        <div className='flex flex-col space-y-2'>
             <button className='bg-blue-700 p-2 font-overpass text-white'>Login with Facebook</button>
             <button className='bg-rose-600 p-2 font-overpass text-white'>Login with Google</button>
         </div>
