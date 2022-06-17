7d527661c16cda9adb7f9ef6438cf79c78ba3342
Obakeng <mosadiobakeng7@gmail.com>
2022-06-17 08:57:22 +0200

    Added login to Facebook button

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -5,6 +5,7 @@ function SignIn() {
     <div className='flex flex-col justify-center items-center'>
         <img src='/images/tobie-logo.png' className='w-24 h-24'/>
         <label className='font-overpass font-bold text-2xl'>Sign In</label>
+        <button>Login with Facebook</button>
     </div>
   )
 }
