1d71f5f2e9f881297a0420fdaf7a5b2fa05dd674
Obakeng <mosadiobakeng7@gmail.com>
2022-06-17 09:00:30 +0200

    Added login with Google button

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -6,6 +6,7 @@ function SignIn() {
         <img src='/images/tobie-logo.png' className='w-24 h-24'/>
         <label className='font-overpass font-bold text-2xl'>Sign In</label>
         <button className='bg-blue-700 p-2 font-overpass text-white'>Login with Facebook</button>
+        <button className='bg-rose-600 p-2 font-overpass text-white'>Login with Google</button>
     </div>
   )
 }
