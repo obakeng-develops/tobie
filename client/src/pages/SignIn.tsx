f9e9a355606f4de463e193a5058124bc3ee20393
Obakeng <mosadiobakeng7@gmail.com>
2022-06-17 09:15:27 +0200

    Removed margin on <hr>

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -9,7 +9,7 @@ function SignIn() {
             <button className='bg-blue-700 p-2 font-overpass text-white'>Login with Facebook</button>
             <button className='bg-rose-600 p-2 font-overpass text-white'>Login with Google</button>
         </div>
-        <hr className='my-5'></hr>
+        <hr></hr>
         <div className='flex flex-col space-y-3'>
             <input type="text" placeholder="Email" className="font-overpass bg-slate-200 p-2"/>
             <input type="password" placeholder="Password" className="font-overpass bg-slate-200 p-2"/>
