f4ebd0686187fcf5946480b364eb5f8223bdb683
Obakeng <mosadiobakeng7@gmail.com>
2022-06-17 08:56:15 +0200

    Added sign in label

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -4,6 +4,7 @@ function SignIn() {
   return (
     <div className='flex justify-center items-center'>
         <img src='/images/tobie-logo.png' className='w-24 h-24'/>
+        <label className='font-overpass font-bold text-2xl'>Sign In</label>
     </div>
   )
 }
