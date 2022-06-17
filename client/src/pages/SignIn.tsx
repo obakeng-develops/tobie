5b737014eff4d56305893a1696f1b55dfb784907
Obakeng <mosadiobakeng7@gmail.com>
2022-06-17 08:56:32 +0200

    Changed flex direction to col

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function SignIn() {
   return (
-    <div className='flex justify-center items-center'>
+    <div className='flex flex-col justify-center items-center'>
         <img src='/images/tobie-logo.png' className='w-24 h-24'/>
         <label className='font-overpass font-bold text-2xl'>Sign In</label>
     </div>
