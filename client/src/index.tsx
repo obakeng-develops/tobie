8dc28159547f93b466676dc6840c19584d797d27
Obakeng <mosadiobakeng7@gmail.com>
2022-06-17 08:51:52 +0200

    Added new route for Sign In

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -9,6 +9,7 @@ import {
 } from "react-router-dom";
 import Dashboard from './pages/Dashboard';
 import Popup from './pages/Popup';
+import SignIn from './pages/SignIn';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -18,6 +19,7 @@ root.render(
     <Routes>
       <Route path="/" element={<App />}/>
       <Route path="/popup" element={<Popup/>}/>
+      <Route path="/signin" element={<SignIn/>}/>
     </Routes>
   </BrowserRouter>
 );
