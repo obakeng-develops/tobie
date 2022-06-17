02b7fc2ae487a7dda90f1212794683df04356f64
Obakeng <mosadiobakeng7@gmail.com>
2022-06-17 09:01:41 +0200

    Added div wrapper around buttons

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -5,8 +5,10 @@ function SignIn() {
     <div className='flex flex-col justify-center items-center'>
         <img src='/images/tobie-logo.png' className='w-24 h-24'/>
         <label className='font-overpass font-bold text-2xl'>Sign In</label>
-        <button className='bg-blue-700 p-2 font-overpass text-white'>Login with Facebook</button>
-        <button className='bg-rose-600 p-2 font-overpass text-white'>Login with Google</button>
+        <div className='flex flex-col'>
+            <button className='bg-blue-700 p-2 font-overpass text-white'>Login with Facebook</button>
+            <button className='bg-rose-600 p-2 font-overpass text-white'>Login with Google</button>
+        </div>
     </div>
   )
 }
